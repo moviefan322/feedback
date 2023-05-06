1c8fe1c83bff9de72ef95fca2d9f142100a332cb
moviefan322 <philman202@yahoo.com>
2023-05-05 21:49:34 -0400

    adds feedbackitem component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div className="container">
+        <h2>Feedback UI</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Header
