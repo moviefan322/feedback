e97d75fab8ae2d4c708b0776c6813bee57219182
moviefan322 <philman202@yahoo.com>
2023-05-06 17:26:04 -0400

    updates to createRoot

---
+++ src/components/Feedbackform.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Card from "./shared/Card";
+
+function Feedbackform() {
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service?</h2>
+
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="write a review"
+            value="text"
+          />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default Feedbackform;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
