659b6534977a42034966c894f409cd599c95b252
moviefan322 <philman202@yahoo.com>
2023-05-06 17:54:54 -0400

    adds form control

--- src/components/Feedbackform.jsx
+++ src/components/Feedbackform.jsx
@@ -4,8 +4,22 @@ import Button from "./shared/Button";
 
 function Feedbackform() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length < 10) {
+      setBtnDisabled(true);
+      setMessage("Your review must be at least 10 characters long");
+    } else {
+        setBtnDisabled(false);
+        setMessage(null);
+    }
+ }
+
     setText(e.target.value);
   };
 
@@ -21,8 +35,11 @@ function Feedbackform() {
             placeholder="write a review"
             value="text"
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Submit
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
